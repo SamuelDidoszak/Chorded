99f972a35b37a5d07845e799a41296ac29686fe0
SamuelDidoszak <samueldidoszak1@gmail.com>
2022-06-13 17:36:10 +0200

    added php routing

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
 
-echo 'Hi there 👋';
+require "Routing.php";
+
+$path = $_SERVER["REQUEST_URI"];
+$path = trim($path, '/');
+
+Routing::get("index", "DefaultController");
+Routing::get("result", "DefaultController");
+
+Routing::run($path);
